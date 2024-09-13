bfdc3f0711adb7f48898d6e52f8f5ca1e93f1791
Sonu Karn <96393941+Sonu-Karn@users.noreply.github.com>
2024-09-14 01:06:56 +0545

    Update new-account-open.php

--- new-account-open.php
+++ new-account-open.php
@@ -1,53 +1,5 @@
 <?php
 
-// session_start();
-
-// $fullname = $_POST['fullname'];
-// $phone = $_POST['phone'];
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-// // $otp = $_POST['otp'];
-
-// // if ($_SESSION['otp'] != $otp || $_SESSION['email'] != $email) {
-// //     die('OTP verification failed or email mismatch');
-// // }
-
-// // Hash the password
-// $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-
-// $con = new mysqli("localhost", "root", "", "user");
-
-// // Check connection
-// if ($con->connect_error) {
-//     die('Failed to connect: ' . $con->connect_error);
-// }
-
-// // Prepare and bind the SQL statement
-// $stmt = $con->prepare('INSERT INTO login (FULLNAME, EMAIL, PASSWORD, PHONE, OTP) VALUES (?, ?, ?, ?, "verified")');
-// if ($stmt === false) {
-//     die('Error preparing statement: ' . $con->error);
-// }
-
-// // Bind parameters
-// $stmt->bind_param('ssss', $fullname, $email, $hashed_password, $phone);
-
-// // Execute the statement
-// if ($stmt->execute()) {
-//     header("Location: http:://localhost/project/log.php");
-//     exit();
-// } else {
-//     echo '<h2>Error registering user: ' . $stmt->error . '</h2>';
-// }
-
-// // Close statement and connection
-// $stmt->close();
-// $con->close();
-
-
-
-
-
-
 // without OTP
 $fullname = $_POST['fullname'];
 $phone = $_POST['phone'];
